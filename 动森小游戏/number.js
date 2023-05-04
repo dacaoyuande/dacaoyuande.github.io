17d6c330ac0eb9e8b177df7021519d9f395b5ab9
dacaoyuande <126841015+dacaoyuande@users.noreply.github.com>
2023-05-04 21:06:35 +0800

    动森小游戏
    创意交互设计期中作业

---
+++ 动森小游戏/number.js
@@ -0,0 +1,25 @@
+ function checkNumber() {
+        const userNumber = Number(numberField.value);
+
+        if (userNumber === 0) {
+          window.open("0.html");
+        } else if (userNumber === 1) {
+          window.open("1.html");
+        } else if (userNumber === 2) {
+          window.open("2.html");
+        } else if (userNumber === 3) {
+          window.open("3.html");
+        } else if (userNumber === 4) {
+          window.open("4.html");
+        } else if (userNumber === 5) {
+          window.open("5.html");
+        } else if (userNumber === 6) {
+          window.open("6.html");
+        } else if (userNumber === 7) {
+          window.open("7.html");
+        } else if (userNumber === 8) {
+          window.open("8.html");
+        } else if (userNumber === 9) {
+          window.open("9.html");
+        } 
+      };
